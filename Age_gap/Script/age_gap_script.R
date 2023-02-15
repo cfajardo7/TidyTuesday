d41fedba9954a7ae761b6121d09dffaf28f96aa7
Cindy Fajardo <cindy.fajardo.625@my.csun.edu>
2023-02-15 12:45:22 -0800

    Made changes to age_gap script

--- Age_gap/Script/age_gap_script.R
+++ Age_gap/Script/age_gap_script.R
@@ -1,4 +1,24 @@
+######################
+# Age Gap TidyTuesday Script#
+# Created by: Cindy Fajardo
+# Created on: 2023_02_15
+
+##############
+#Libraries
+
+library(tidyverse)
 library(readr)
-urlfile="https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-14/age_gaps.csv"
-mydata <- read_csv(url(urlfile))
+library(here)
+library(nationalparkcolors)
+library(praise)
+library(devtools)
+library(beyonce)
+library(ggthemes)
+library(hrbrthemes)
+
+#################
+#Beggining of Script
+
+urlfile="https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-14/age_gaps.csv" #names url file
+mydata <- read_csv(url(urlfile)) #read in csv from hithub using url file
 View(mydata)
