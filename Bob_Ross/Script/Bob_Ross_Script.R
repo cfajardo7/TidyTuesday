d378298d63687ae4e791152876a0afc207761fbe
Cindy Fajardo <cindy.fajardo.625@my.csun.edu>
2023-04-24 21:41:34 -0700

    saved output

--- Bob_Ross/Script/Bob_Ross_Script.R
+++ Bob_Ross/Script/Bob_Ross_Script.R
@@ -21,20 +21,20 @@ library(nationalparkcolors)
 library(tidytuesdayR)
 library(ggplot2)
 library(viridis)
+library(BobRossColors)
 
 #Load Data
 bob_ross <- read_csv(
   "https://raw.githubusercontent.com/jwilber/Bob_Ross_Paintings/master/data/bob_ross_paintings.csv") #load the data in
 
-bob_ross <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-21/bob_ross.csv') #read the data in
 View(bob_ross)
 
 colors_used <- bob_ross %>% 
   select(season, num_colors, Black_Gesso, Bright_Red, Burnt_Umber, Cadmium_Yellow, Dark_Sienna, Indian_Red, Indian_Yellow, Liquid_Black, Liquid_Clear, Midnight_Black, Phthalo_Blue, Phthalo_Green, Prussian_Blue, Sap_Green, Titanium_White, Van_Dyke_Brown, Yellow_Ochre, Alizarin_Crimson) %>% #select what I want to use in dataframe
-  filter(season %in% c(1,2,3,4,5,6,7,8,9,10)) %>% #filtered to only show season 1-10
-  pivot_longer(cols = Black_Gesso:Alizarin_Crimson, #pivoted longer to ger colors in one group
-               names_to = "colors_used",
-               values_to = "color_present")
+  filter(season %in% c(1,2,3,4,5)) %>% #filtered to only show season 1-5
+  pivot_longer(cols = Black_Gesso:Alizarin_Crimson, #pivoted longer to get colors in one group
+               names_to = "colors_used", #new column with names
+               values_to = "color_present") #new colums with values
 View(colors_used)
 
 ggplot(colors_used,
@@ -42,7 +42,11 @@ ggplot(colors_used,
              y = num_colors,
              fill = colors_used))+
   geom_bar(stat = "identity", width = 0.5)+
-  scale_fill_viridis_d()
-#I didn't get what I wanted. I am not sure what is going on on the y axis. It maybe because I am using the color used as a fill. I need help
+  scale_fill_bob_ross(painting = "autumn_splendor")+
+  theme_dark()+
+  labs(title = "Amount of Different Colors Used Per Season",
+       x = "Season", # x axis title
+       y = "Number of Colors Used")+ # y axis title
+  guides(fill=guide_legend(title="Colors")) #changes legend when using fill instead of color
 
-
+ggsave(here("Bob_Ross","Output","bob_ross.jpg"))
